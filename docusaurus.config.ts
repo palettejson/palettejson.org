39873297cf69ef0cb4ebeb318e897d7f5e342400
pmdci <pmdci@users.noreply.github.com>
2025-09-13 04:22:45 +0100

    Initial commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/palettejson/palettejson.org/",
+          editUrl: "https://github.com/palettejson/palettejson.org/blob/main/",
         },
         blog: false,
         theme: {
