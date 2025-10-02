3b0795aa501103d452954741d133c3bc18ef652a
pmdci <pmdci@users.noreply.github.com>
2025-10-02 13:58:23 +0100

    Made sidebar hideable

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -107,6 +107,11 @@ const config: Config = {
         },
       ],
     },
+    docs: {
+      sidebar: {
+        hideable: true,
+      },
+    },
     footer: {
       style: "dark",
       links: [
